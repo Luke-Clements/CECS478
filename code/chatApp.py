65dc31951386472da9c38330460e36dbd6189ae7
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-10-15 21:07:40 -0700

    updated chatApp

--- code/chatApp.py
+++ code/chatApp.py
@@ -1,11 +1,12 @@
 import os
-from allEncryption import messageEncryptMAC
-from fileManagement import saveFileAsJSON
-from allDecryption import fileDecrypt
+from allEncryption import RSAEncrypt
+from fileManagement import saveMessageAsJSON, loadMessageFromJSON
+from allDecryption import RSADecrypt
 from getMessage import getMessage
+from keyPaths import keyPaths
 
 def flowControl():
-    # bool keepChatting = True
+
     while True:
         userContinue = getMessage("Would you like to keep chatting(Y/N)? ").lower()
 
@@ -16,14 +17,22 @@ def flowControl():
         # retrieve person and chat from local
             # user should have option to read, but not send any additional message
         message = getMessage("Luke").encode()
+        # message.encode("utf-8")
 
-        ciphertext, iv, tag, ENCKey, HMACKey = messageEncryptMAC(message)
-        # print("Ciphertext: " + ciphertext.decode() + "\n")
+        print(message)
+        RSACipher, ciphertext, iv, tag = RSAEncrypt(message, keyPaths.pathToPublicKey)
+        print(ciphertext)
 
         #for testing purposes only
         testDir = os.getcwd() + "/testDir"
 
-        saveFileAsJSON(testDir + "/message", ciphertext, iv, tag, ENCKey, HMACKey)
+        # print(type(ciphertext))
+        # need to come up with a message naming system to append to "message"
+        saveMessageAsJSON(testDir + "/m.json", RSACipher, ciphertext, iv, tag)
 
-        messageDecrypted, ext = fileDecrypt(workingDir)
         # send message to person
+
+        # for testing only
+        RSACipher, text, iv, tag = loadMessageFromJSON(testDir + "/m.json")
+        messageDecrypted = RSADecrypt(RSACipher, text, iv, tag, keyPaths.pathToPrivateKey)
+        print(messageDecrypted)
