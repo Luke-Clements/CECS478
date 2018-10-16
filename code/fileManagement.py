b5f6203f79145f788f797919a7e061a103bdad77
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-10-15 21:08:12 -0700

    updated fileManagement methods

--- code/fileManagement.py
+++ code/fileManagement.py
@@ -2,6 +2,31 @@ import json
 
 from base64 import b64decode, b64encode
 
+def loadMessageFromJSON(filepath):
+
+    with open(filepath, 'r') as json_file:
+        data = json.load(json_file)
+    
+    key = b64decode(data["Key"])
+    iv = b64decode(data["IV"])
+    text = b64decode(data["Text"])
+    tag = b64decode(data["Tag"])
+    json_file.close()
+    return (key, text, iv, tag)
+
+def saveMessageAsJSON(saveFilePath, key, text, iv, tag):
+
+    data = {
+        'IV': b64encode(iv).decode('utf-8'),
+        'Key': b64encode(key).decode('utf-8'),
+        'Text': b64encode(text).decode('utf-8'),
+        'Tag': b64encode(tag).decode('utf-8')
+        }
+
+    with open(saveFilePath, 'w+') as outFile:
+        json.dump(data, outFile)
+    outFile.close()
+
 def loadFileFromJSON(filepath):
     with open(filepath, 'r') as json_file:
         data = json.load(json_file)
