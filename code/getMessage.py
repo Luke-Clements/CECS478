15e6965c9609e9c2e9e83090b3dc344918109156
MarcM1250 <36463798+MarcM1250@users.noreply.github.com>
2018-10-15 23:42:36 -0700

    Update getMessage.py

--- code/getMessage.py
+++ code/getMessage.py
@@ -1,6 +1,6 @@
 import os
 
 def getMessage(person):
-    message = input("Message to " + person + ": ")
+    message = input("Message to " + person + ": ") # User Input, enter person
 
-    return message
+    return message # Return collected user input
