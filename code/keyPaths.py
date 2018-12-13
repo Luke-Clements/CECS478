6b3d707683a893e72df5f0e4163b1f4cc81c92fd
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-12-13 13:01:06 -0800

    Delete keyPaths.py

--- code/keyPaths.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class keyPaths:
-    pathToPublicKey = os.getcwd() + '/.ssh/rsaKey.pem.pub'  # directory to public .pem
-    pathToPrivateKey = os.getcwd() + '/.ssh/rsaKey.pem'     # directory to private .pem
-
