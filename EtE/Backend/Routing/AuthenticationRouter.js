e7993c583619dac499e1e0f99fa51081e22e6e0b
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-12-13 14:37:16 -0800

    Delete AuthenticationRouter.js

--- EtE/Backend/Routing/AuthenticationRouter.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-    var authenticationController = require("../Controllers/AuthenticationController");
-
-//Authenticate a user
-app.route('/authenticate')
-    .post(authenticationController.authenticate);
-}
